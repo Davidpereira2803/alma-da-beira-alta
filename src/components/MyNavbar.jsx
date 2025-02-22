0f7150ae9c9fd544eb477eb21d13b3ff747649c9
DavidPereira2803 <david.pereira.002@student.uni.lu>
2025-02-22 21:10:02 +0100

    fix: update fallback language to Portuguese and change logo file format to SVG

--- src/components/MyNavbar.jsx
+++ src/components/MyNavbar.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import logo from "../assets/logo.jpg";
+import logo from "../assets/logo.svg";
 import { useTranslation } from "react-i18next";
 
 function MyNavbar() {
